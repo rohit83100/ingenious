95ffc817bef4a97f420c51c360739c1d63b5a429
rohitk <rohitk@thebigrichgroup.com>
2022-08-17 14:31:34 +0530

    progress

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,44 @@
+class PostController < ApplicationController
+    before_action :authenticate_user!
+    def index
+        @post = Post.all
+    end   
+    def show
+        @post = Post.find(params[:id])
+    end 
+    def new
+        @post = post.new
+    end    
+    def edit
+        @post = Post.find(params[:id])
+    end
+    def create
+        @post = Post.new(post_params)
+ 
+        if @post.save
+          redirect_to @post
+        else
+          render 'new'
+        end
+    end   
+    def update
+        @post = Post.find(params[:id])
+       
+        if @post.update(post_params)
+          redirect_to @post
+        else
+          render 'edit'
+        end
+    end
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to post_index_path
+    end
+
+
+    private
+    def post_params
+        params.require(:post).permit(:description)
+    end 
+end
---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+  }
+  devise_for :views
+  resources :post
+  get 'homepage/index'
+  get 'homepage/about'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "homepage#index"
+end
